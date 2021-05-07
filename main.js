ea171b901293153464c420f653254ccc0161db92
Pratyush-9 <83753580+Pratyush-9@users.noreply.github.com>
2021-05-07 12:57:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg","https://i.postimg.cc/L6HcYQM0/grand-father.png", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg",  "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-            var names = ["Fmaily Book","Prabhat Jain", "Prateek Jain", "Pratyush Jain",  "Shruti Jain"];
-            var i = 0;
-            function update()
-            {
-                i++;
-                var numbers_of_family_member_in_array = 5
-                if(i > numbers_of_family_member_in_array )
-                {
-                    i = 0;
-                }
-                var updatedImage = images[i];
-                var updatedName  = names[i];
-                document.getElementById("family_member_image").src = updatedImage;
-                document.getElementById("family_member_name").innerHTML = updatedName;
-            }
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg",
+ "https://i.postimg.cc/L6HcYQM0/grand-father.png",
+ "https://i.postimg.cc/wjMnFtMX/father.jpg",
+  "https://i.postimg.cc/5ymDKL83/bro.jpg", 
+  "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+  
+var names = ["Fmaily Book", "Prabhat Jain", "Prateek Jain", "Pratyush Jain", "Shruti Jain"];
+var i = 0;
+
+function update() {
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if (i > numbers_of_family_member_in_array) {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
